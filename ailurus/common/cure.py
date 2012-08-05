7316a279b43028603885fc862c854129b70cbb9b
Homer Hsing <homer.hsing@gmail.com>
2012-08-05 20:02:17 +0800

    drop uncommon cure

--- ailurus/common/cure.py
+++ ailurus/common/cure.py
@@ -20,28 +20,6 @@ from __future__ import with_statement
 import sys, os
 from lib import *
 
-class Autostart_Workrave(C):
-    __doc__ = _('Automatically start up Workrave')
-    detail = _('Create file:') + ' ' + os.path.expanduser('~/.config/autostart/workrave.desktop')
-    path = os.path.expanduser('~/.config/autostart/')
-    file = path + 'workrave.desktop'
-    def exists(self):
-        if UBUNTU or UBUNTU_DERIV:
-            return APT.installed('workrave') and not os.path.exists(self.file)
-        if FEDORA:
-            return RPM.installed('workrave') and not os.path.exists(self.file)
-    def cure(self):
-        if not os.path.exists(self.path):
-            os.mkdir(self.path)
-        with open(self.file, 'w') as f:
-            f.write('[Desktop Entry]\n'
-                    'Name=Workrave\n'
-                    'Exec=workrave\n'
-                    'Encoding=UTF-8\n'
-                    'Version=1.0\n'
-                    'Type=Application\n'
-                    'X-GNOME-Autostart-enabled=true\n')
-
 class Create_basic_vimrc(C):
     __doc__ = _('Create basic VIM configuration file (.vimrc)')
     detail = _('File content:') + ' syntax on; set autoindent; set number; set mouse=a'
@@ -58,44 +36,6 @@ class Create_basic_vimrc(C):
                     'set number\n'
                     'set mouse=a\n')
 
-class Create_Imagemagick_shortcut(C):
-    __doc__ = _('Create ImageMagick shortcut in "Application->Graphics" menu')
-    file = '/usr/share/applications/imagemagick.desktop'
-    detail = _('Create file:') + ' ' + file
-    def exists(self):
-        if UBUNTU or UBUNTU_DERIV:
-            return APT.installed('imagemagick') and not os.path.exists(self.file)
-        if FEDORA:
-            return RPM.installed('ImageMagick') and not os.path.exists(self.file)
-    def cure(self):
-        create_file(self.file, '[Desktop Entry]\n'
-                               'Name=ImageMagick\n'
-                               'Exec=display %f\n'    
-                               'Encoding=UTF-8\n'
-                               'StartupNotify=true\n'
-                               'Terminal=false\n'
-                               'Type=Application\n'
-                               'Categories=GNOME;GTK;Graphics;\n')
-
-class Create_softlink_to_desktop_folder(C):
-    __doc__ = _('Create a directory "Desktop" linked to the desktop in your home folder')
-    detail = _('After that, you can easily chdir to desktop folder by command "cd $HOME/Desktop".')
-    desktop = os.path.expanduser('~/Desktop')
-    def exists(self):
-        return not os.path.exists(self.desktop)
-    def cure(self):
-        with open(os.path.expanduser('~/.config/user-dirs.dirs')) as f:
-            contents = f.readlines()
-        name = None
-        for line in contents:
-            if line.startswith('#'): continue
-            if 'XDG_DESKTOP_DIR' in line:
-                name = line.split('=')[1].strip()
-                if name.startswith('"') and name.endswith('"'): name = name[1:-1]
-                name = os.path.expandvars(name)
-        if name and os.path.exists(name):
-            run('ln -s %s %s'%(name, self.desktop))
-
 class Query_before_remove_a_lot_of_files(C) :
     __doc__ = _('Query you before delete more than three files in BASH')
     detail = _('Prevent destruction when you mistype "rm subdir/*" as "rm subdir/ *".\n'
@@ -106,80 +46,3 @@ class Query_before_remove_a_lot_of_files(C) :
         return not file_contain(self.bashrc, self.line)
     def cure(self):
         file_append(self.bashrc, self.line)
-
-# This class needs improvement
-#
-#class Speed_Up_Firefox(I):
-#    __doc__ = _('Speed up Firefox')
-#    detail = _('Firefox is faster when Pango rendering is disabled. '
-#        'The trick is to launch Firefox by the command: "export MOZ_DISABLE_PANGO=1; firefox". '
-#        'Ailurus will create a new icon "Firefox without Pango (faster)" in the menu "Applications"-->"Internet".')
-#    def install(self):
-#        paths = [
-#                 '/usr/share/applications/firefox-3.5.desktop',
-#                 '/usr/share/applications/firefox.desktop', 
-#                 '/usr/share/applications/mozilla-firefox.desktop',
-#                 '/usr/share/applications/abrowser.desktop',
-#                 ]
-#        for path in paths:
-#            import os
-#            if os.path.exists(path): break
-#        else:
-#            raise Exception('Firefox shortcut is not found.')
-#            
-#        with open(path) as f:
-#            content = f.readlines()
-#        for i, line in enumerate(content):
-#            if line.startswith('Exec='):
-#                firefox_launcher = line.split('=')[1].strip()
-#                new = 'Exec=sh -c "export MOZ_DISABLE_PANGO=1; %s"\n'%firefox_launcher
-#                content[i] = new
-#            if line.startswith('Name='):
-#                content[i] = 'Name=%s\n'%_('Firefox without Pango (faster)')
-#        dir = '/usr/local/share/applications/'
-#        if not os.path.exists(dir): run_as_root('mkdir ' + dir)
-#        with TempOwn(dir + 'firefox.nopango.desktop'):
-#            with open(dir + 'firefox.nopango.desktop', 'w') as f:
-#                f.writelines(content)
-#
-#    def installed(self):
-#        import os 
-#        return ( os.path.exists('/usr/local/share/applications/firefox.nopango.desktop') or
-#                 os.path.exists('/usr/share/applications/firefox.nopango.desktop') )
-#    def remove(self):
-#        run_as_root('rm -f /usr/local/share/applications/firefox.nopango.desktop')
-#        run_as_root('rm -f /usr/share/applications/firefox.nopango.desktop')
-
-class Own_usr_lib_eclipse_by_root(C):
-    __doc__ = _('Let root own /usr/lib/eclipse and /usr/share/eclipse')
-    detail = _('Otherwise, Pydev and CDT do not work.')
-    def wrong(self, path):
-        if os.path.exists(path):
-            statinfo = os.stat(path)
-            if statinfo.st_uid != 0 or statinfo.st_gid != 0:
-                return True
-        return False
-    def exists(self):
-        return self.wrong('/usr/lib/eclipse') or self.wrong('/usr/share/eclipse')
-    def cure(self):
-        run_as_root('chown -R root:root /usr/lib/eclipse', ignore_error=True)
-        run_as_root('chown -R root:root /usr/share/eclipse', ignore_error=True)
-
-class Own_config_dir_by_user(C):
-    __doc__ = _('Let you be the owner of directory $HOME/.config/ailurus')
-    detail = _('Command:') + ' chown -R $USER:$USER $HOME/.config/ailurus'
-    type = C.MUST_FIX
-    def exists(self):
-        dir = Config.config_dir
-        if os.stat(dir).st_uid != os.getuid():
-            return True
-    def cure(self):
-        run_as_root('chown -R $USER:$USER "%s"' % Config.config_dir)
-
-class Completely_remove_var_cache_ailurus(C):
-    __doc__ = _('Remove directory /var/cache/ailurus/')
-    detail = _('This directory is obsolete. Remove this directory to free disk space.')
-    def exists(self):
-        return os.path.exists('/var/cache/ailurus')
-    def cure(self):
-        run_as_root('rm -rf /var/cache/ailurus')
